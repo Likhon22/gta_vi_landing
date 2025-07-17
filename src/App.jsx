2a04cd5cb41007791b61488a2d5e47d1cba155c9
Likhon22 <likhonsarker793@gmail.com>
2025-07-18 01:26:05 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+function App() {
+  return <h2 className=" text-white">Hello</h2>;
+}
+
+export default App;
